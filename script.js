4993f617d9916dd0a1948d374ad37ca93defc376
Adrian Piwin <adrian7048@gmail.com>
2021-12-07 17:04:00 -0500

    Navbar and Projects Page
    - Added navbar with nice animation
    - Added projects page with layout

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-function onNavClick(){
-    console.log("test");
+function openNav(){
+    document.getElementById("mySidenav").style.transform = "translate(-250px)";
+}
+
+function closeNav(){
+    document.getElementById("mySidenav").style.transform = "translate(0px)";
 }
